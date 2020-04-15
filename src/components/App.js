3bb9eccbce31b4849656325e56f18eb1c8978bef
Kim Sangyeon <dho113@naver.com>
2020-04-15 17:39:58 +0900

    Creating movie web apps with GraphQL - #0.3 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import Details from '../routes/Details';
 
 function App() {
-  return <div className="App">Apollo 2020</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Details} />
+    </Router>
+  );
 }
 
 export default App;
