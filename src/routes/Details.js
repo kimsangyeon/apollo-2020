29fcdda16c414a94e14cb620fe3d75e3d72fc4b4
Kim Sangyeon <dho113@naver.com>
2020-04-19 21:13:27 +0900

    Creating movie web apps with GraphQL - #1.2 GET_MOVIE Query

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,3 +1,30 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
 
-export default () => <h1>Details</h1>;
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      summary
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  if (data && data.movie) {
+    return <h1>hdata.movie.title</h1>;
+  }
+};
