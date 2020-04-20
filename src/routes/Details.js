f011a416ed15ad7cb9760ec715551b2d261136a4
Kim Sangyeon <dho113@naver.com>
2020-04-20 23:52:38 +0900

    Creating movie web apps with GraphQL - #1.3 Apollo Cache and Styles

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -9,9 +10,45 @@ const GET_MOVIE = gql`
       id
       title
       medium_cover_image
-      summary
+      language
+      rating
+      description_intro
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 export default () => {
@@ -20,11 +57,14 @@ export default () => {
     variables: { id: parseInt(id) },
   });
 
-  if (loading) {
-    return 'loading...';
-  }
-
-  if (data && data.movie) {
-    return <h1>hdata.movie.title</h1>;
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -48,6 +48,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
   console.log(loading, error, data);
@@ -59,7 +68,13 @@ export default () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading && data.movies && data.movies.map((m) => <Movie id={m.id} key={m.id} />)}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie id={m.id} key={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   );
 };
